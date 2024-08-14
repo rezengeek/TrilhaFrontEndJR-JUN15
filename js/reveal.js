8bdbaed6b7ae56d3698bae07ffe7430739ce23d8
Oscar Rezende <netox2002@gmail.com>
2024-08-14 02:16:04 -0300

    feat: Scroll Reveal

---
+++ js/reveal.js
@@ -0,0 +1,36 @@
+window.effectreveal = ScrollReveal({reset:true})
+
+// About My self
+
+effectreveal.reveal('.about-myself h1', 
+{
+  duration: 2000,
+  distance: '90px',
+  reset: false
+})
+
+effectreveal.reveal('.effect-text-about', 
+{
+  duration: 2000,
+  distance: '90px',
+  delay: 500,
+  origin: 'left',
+  reset: false
+})
+
+effectreveal.reveal('.effect-photo-about', 
+{
+  duration: 2000,
+  distance: '90px',
+  delay: 500,
+  origin: 'right',
+  reset: false
+})
+
+effectreveal.reveal('.effect-icons-about', 
+{
+  duration: 2000,
+  distance: '90px',
+  delay: 700,
+  reset: false
+})
