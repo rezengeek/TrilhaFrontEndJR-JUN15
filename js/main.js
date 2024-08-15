d1fe838d7973ec3fc5059416215b2443e2375fd5
Oscar Rezende <netox2002@gmail.com>
2024-08-14 21:34:53 -0300

    feat: Scroll to top

--- js/main.js
+++ js/main.js
@@ -55,3 +55,23 @@ let lastScrollTop = 0;
     document.querySelector('.video-video').style.display = 'block';
 }
 
+// função scroll to top
+
+$(function(){
+  $(document).on( 'scroll', function(){
+      if ($(window).scrollTop() > 100) {
+          $('.smoothscroll-top').addClass('show');
+      } else {
+          $('.smoothscroll-top').removeClass('show');
+      }
+  });
+  $('.smoothscroll-top').on('click', scrollToTop);
+});
+
+function scrollToTop() {
+  verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+  element = $('body');
+  offset = element.offset();
+  offsetTop = offset.top;
+  $('html, body').animate({scrollTop: offsetTop}, 600, 'linear').animate({scrollTop:25},200).animate({scrollTop:0},150) .animate({scrollTop:0},50);
+}
